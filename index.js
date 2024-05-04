23d565d53e507c1d11bd3e933df4e1b8037a5cfd
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-05-04 16:18:55 +1000

    selecting the elements

---
+++ index.js
@@ -0,0 +1,10 @@
+// selection of the elements required for the actions
+
+const alertMessage = document.querySelector('.alert-msg');
+const input = document.getElementById('grocery');
+const toDo = document.querySelector('.todo-app');
+const submit = document.querySelector('.btn-submit');
+const listContainer = document.querySelector('.list-container');
+const list = document.querySelector('.list');
+const clear = document.querySelector('.clear-items');
+// end of selection of elements
