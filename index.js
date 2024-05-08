e4965a3983ad0f42f035cce49fe86aad7caa98cd
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-05-08 10:19:07 +1000

    editing the local storage and retriving from localstorage after refresh

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ let editID = '';
 // first for form submit which is toDo in our case:
 toDo.addEventListener('submit', addItem);
 clear.addEventListener('click', clearItem);
+window.addEventListener('DOMContentLoaded', setUpItems);
 
 // const deleteButton = document.querySelector('.delete');
 
@@ -30,38 +31,9 @@ function addItem(e) {
 
   const id = new Date().getTime().toString();
   if (inputValue && !editFlag) {
-    // console.log('add to the list');
-
-    const tobeAddedElement = document.createElement('article');
-    tobeAddedElement.classList.add('list-items'); // adding the class to the article
-
-    // adding id
-    const attr = document.createAttribute('data-id');
-    attr.value = id;
-    tobeAddedElement.setAttributeNode(attr);
-    tobeAddedElement.innerHTML = ` <span class="title">${inputValue}</span>
-    <div class="btn-container">
-      <button type="button" class="edit">
-        <i class="fa-solid fa-pen-to-square"></i>
-      </button>
-      <button type="button" class="delete">
-        <i class="fa-solid fa-trash"></i>
-      </button>
-    </div>`;
-
-    // adding to the list by appending child
-
-    list.appendChild(tobeAddedElement);
+    console.log('add to the list');
+    createListItems(id, inputValue);
     alertDisplay('item added to list', 'success');
-    listContainer.classList.add('show-container');
-
-    //showing container of list
-
-    const deleteButton = tobeAddedElement.querySelector('.delete');
-    const editButton = tobeAddedElement.querySelector('.edit');
-
-    deleteButton.addEventListener('click', deleteItem);
-    editButton.addEventListener('click', editItem);
     //addToLocalStorage
     addToLocalStorage(id, inputValue);
     setBackToDefault();
@@ -90,6 +62,7 @@ function clearItem() {
   listContainer.classList.remove('show-container');
   alertDisplay('empty list', 'danger');
   setBackToDefault();
+  localStorage.removeItem('list');
 }
 
 // delete function
@@ -107,7 +80,7 @@ function deleteItem(e) {
 
   // removing from local storage
 
-  //   removeFromLocalStorage(id);
+  removeFromLocalStorage(id);
 }
 
 // edit function
@@ -116,7 +89,7 @@ function editItem(e) {
   const element = e.currentTarget.parentElement.parentElement;
 
   // console.log(e.currentTarget);
-  console.log(element);
+  // console.log(element);
   // editElement = e.currentTarget.parentElement.previousSiblingElement;//The difference between this property and previousSibling, is that previousSibling returns the previous sibling node as an element node, a text node or a comment node, while previousElementSibling returns the previous sibling node as an element node (ignores text and comment nodes).
 
   editElement = e.currentTarget.parentElement.previousElementSibling;
@@ -125,8 +98,7 @@ function editItem(e) {
   editFlag = 'true';
   editID = element.dataset.id;
   submit.textContent = 'edit';
-
-  console.log(editElement);
+  // console.log(editElement);
 }
 // Alert display function
 function alertDisplay(text, action) {
@@ -151,7 +123,84 @@ function setBackToDefault() {
 }
 
 //localStorage
-function addToLocalStorage(id, value) {
-  //   console.log('added to addToLocalStorage');
+function addToLocalStorage(id, inputValue) {
+  console.log('added to addToLocalStorage');
+  const input = { id, inputValue };
+  console.log(input);
+  let items = getLocalStorage();
+  // console.log(items);
+  items.push(input);
+  localStorage.setItem('list', JSON.stringify(items));
+  console.log(items);
 }
-function removeFromLocalStorage(id) {}
+function removeFromLocalStorage(id) {
+  let items = getLocalStorage();
+  items = items.filter((item) => {
+    if (item.id !== id) {
+      return item;
+    }
+  });
+}
+function editLocalStorage(id, value) {
+  let items = getLocalStorage();
+  items = items.map(function (item) {
+    if (item.id === id) {
+      item.inputValue = value;
+    }
+    return item;
+  });
+  localStorage.setItem('list', JSON.stringify(items));
+}
+function getLocalStorage() {
+  return localStorage.getItem('list')
+    ? JSON.parse(localStorage.getItem('list'))
+    : [];
+}
+
+function setUpItems(id, value) {
+  let items = getLocalStorage();
+  if (items.length > 0) {
+    items.forEach((item) => {
+      createListItems(item.id, item.inputValue);
+    });
+    listContainer.classList.add('show-container');
+  }
+}
+
+function createListItems(id, inputValue) {
+  const tobeAddedElement = document.createElement('article');
+  tobeAddedElement.classList.add('list-items'); // adding the class to the article
+
+  // adding id
+  const attr = document.createAttribute('data-id');
+  attr.value = id;
+  tobeAddedElement.setAttributeNode(attr);
+  tobeAddedElement.innerHTML = ` <span class="title">${inputValue}</span>
+    <div class="btn-container">
+      <button type="button" class="edit">
+        <i class="fa-solid fa-pen-to-square"></i>
+      </button>
+      <button type="button" class="delete">
+        <i class="fa-solid fa-trash"></i>
+      </button>
+    </div>`;
+
+  // adding to the list by appending child
+
+  list.appendChild(tobeAddedElement);
+
+  listContainer.classList.add('show-container');
+
+  //showing container of list
+
+  const deleteButton = tobeAddedElement.querySelector('.delete');
+  const editButton = tobeAddedElement.querySelector('.edit');
+
+  deleteButton.addEventListener('click', deleteItem);
+  editButton.addEventListener('click', editItem);
+}
+// localStorage.setItem('orange', JSON.stringify(['item', 'item1']));
+// let oranges = JSON.parse(localStorage.getItem('orange'));
+// localStorage.removeItem('orange');
+
+// console.log(oranges);
